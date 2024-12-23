ce350440cd626c878b751e0238941911472a0a8f
unknown <pugazhselvan03@gmail.com>
2024-12-23 19:02:45 +0530

    made loading screen in chatbot

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,9 +1,25 @@
-import loading_gif from "../assests/loading.gif"
+import loading_gif from "../assests/loading.gif";
 
 const Loader = () => (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh',zIndex: "1" }}>
-        <img src={loading_gif} alt="Loading..." />
+  <div
+    style={{
+      position: "absolute", // To ensure the loader is on top
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      backgroundColor: "rgba(0, 0, 0, 0.3)", // Semi-transparent dark background
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      zIndex: 1, // Loader on top
+      borderRadius: "10px", // Optional: rounded corners
+    }}
+  >
+    <div style={{ textAlign: "center" }}>
+      <img src={loading_gif} alt="Loading..." />
     </div>
+  </div>
 );
 
 export default Loader;
