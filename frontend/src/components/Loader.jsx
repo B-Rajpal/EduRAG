396fc7ba4148f190e766fed5b73771e1adca20ab
RIN-NOHARA <126176694+RIN-NOHARA@users.noreply.github.com>
2024-12-26 13:31:20 +0530

    Update Loader.jsx

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,9 +1,10 @@
 import loading_gif from "../assests/loading.gif"
 
 const Loader = () => (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh',zIndex: "1" }}>
+    <div className="flex-center height-10vh z-index-1">
         <img src={loading_gif} alt="Loading..." />
     </div>
 );
+
 
 export default Loader;
